b3a1e4e023ccca011b96027ff3b37b34d32c5fb5
sakib <isakib@gmail.com>
2015-02-22 15:19:48 +0600

    new files changed

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,17 @@
-class VisitorsController < ApplicationController
+# class VisitorsController < ApplicationController
+#
+#   def new
+#     @owner = Owner.new
+#   end
+# end
+#
+#
 
+
+class VisitorsController < ApplicationController
   def new
+    Rails.logger.debug 'DEBUG: entering new method'
     @owner = Owner.new
+    Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
   end
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -9,6 +9,7 @@ class Owner
   end
 
   def countdown
+    Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + 'will appear in bold blue'
     today = Date.today
     birthday = Date.new(today.year, birthdate.month, birthdate.day)
     if birthday > today
